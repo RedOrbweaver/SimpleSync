8449435a3735318177308d846c0195babd679f04
RedOrbweaver <oliwier.ciesielski@proton.me>
2025-10-24 12:34:29 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleSync
+{
+    static class SimpleSync
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            return 0;
+        }
+    }
+}
